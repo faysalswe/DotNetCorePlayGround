8699a0a00ebfeb605a21e4f30a831222f7e63e5d
Faysal Ahmmad <faysal.ahmmad@bs-23.net>
2020-06-17 21:03:34 +0600

    encapsulation

---
+++ AdvanceCSharpTopic/OOP/Encapsulation/AccessModifires.cs
@@ -0,0 +1,18 @@
+﻿namespace AdvanceCSharpTopic
+{
+    // default modifier is internal for class
+    // private for everything else
+    class AccessModifires
+    {
+        private class EncapsulationClassPrivate { }
+
+        protected class EncapsulationClassProtected { }
+
+        internal class EncapsulationClassInternal { }
+
+        protected internal class EncapsulationClassProtectedInternal { }
+
+        private protected class EncapsulationClassPrivateProtected { }
+
+    }
+}
---
+++ AdvanceCSharpTopic/OOP/Encapsulation/Encapsulation.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * public => visible to everyone.
+ * private => only visible containing class .
+ * protected => only visible to containing class and inheritors.
+ * internal => only visible to classes in the same assembly
+ * protected internal => containing assembly public and other assembly as protected.
+ * private protected => containing assembly protected and other assembly as private.
+ */
+
+namespace AdvanceCSharpTopic.OOP
+{
+    internal class Encapsulation: AccessModifires
+    {
+        EncapsulationClassInternal objInternal;
+        EncapsulationClassProtected objProtected; 
+        EncapsulationClassProtectedInternal objProtectedInternal; 
+        EncapsulationClassPrivateProtected objPrivateProtected;
+    }
+
+    /* Can be done  */
+
+    internal class ClassPublic1 : AccessModifires { }
+
+    internal class ClassPublic2 : AccessModifires.EncapsulationClassInternal { }
+
+    internal class ClassPublic3 : AccessModifires.EncapsulationClassProtectedInternal { }
+
+
+    /* Can't be done */
+
+    //public class ClassPublic1 : Encapsulation {}
+
+    //public class ClassPublic2 : Encapsulation.EncapsulationClassInternal {}
+
+    //public class ClassPublic3 : Encapsulation.EncapsulationClassProtectedInternal {}
+}
